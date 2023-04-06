4ac6d785aaf728cd411918d719fa1a8662908209
Rohit Kumar <91238263+Rohitkumar20011806@users.noreply.github.com>
2023-04-06 05:56:24 +0530

    Add files via upload

---
+++ Compiler_Design_Lex_Programs-main/Compiler_Design_Lex_Programs-main/input.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    // Single line comment
+    int a = 10;
+    /* fgfhh
+        This is Multiline Comment
+        This is Multiline Comment
+        This is Multiline Comment
+        This is Multiline Comment
+    */
+    printf("%d",a) ;
+    return 0 ;
+}
